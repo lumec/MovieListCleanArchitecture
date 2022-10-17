de837f3eff235fb103e16bf0435234c9f527e8c5
lumec <lumec@engineer.com>
2022-10-17 16:27:07 -0500

    Add navigation component

--- app/src/main/java/com/labi2d/challenge/moviestwo/MainActivity.kt
+++ app/src/main/java/com/labi2d/challenge/moviestwo/MainActivity.kt
@@ -2,10 +2,44 @@ package com.labi2d.challenge.moviestwo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration : AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.navigation_activity)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navHostFragment: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.home_dest, R.id.movies_dest, R.id.series_dest)
+        )
+
+        setupActionBar(navController, appBarConfiguration)
+        setupBottomNavMenu(navController)
+
+    }
+
+    private fun setupActionBar(navController: NavController, appBarConfig : AppBarConfiguration) {
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNav?.setupWithNavController(navController)
     }
 }
