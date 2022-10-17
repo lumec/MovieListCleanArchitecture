5ee9084cd43708d8e2fe291a030851d994bc61bb
lumec <lumec@engineer.com>
2022-10-17 13:37:33 -0500

    first commit

---
+++ app/src/main/java/com/labi2d/challenge/moviestwo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.labi2d.challenge.moviestwo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
