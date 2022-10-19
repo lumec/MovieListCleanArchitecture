a8e4c036ad0633e3cc3ec844ea8532e02bd844ce
lumec <lumec@engineer.com>
2022-10-19 14:38:03 -0500

    Use ListAdapter for simple films

--- app/src/main/java/com/labi2d/challenge/moviestwo/ui/FilmsAdapter.kt
+++ app/src/main/java/com/labi2d/challenge/moviestwo/ui/FilmsAdapter.kt
@@ -1,32 +1,34 @@
 package com.labi2d.challenge.moviestwo.ui
 
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.labi2d.challenge.moviestwo.R
+import com.labi2d.challenge.moviestwo.basicDiffUtil
+import com.labi2d.challenge.moviestwo.databinding.FilmItemViewBinding
+import com.labi2d.challenge.moviestwo.inflate
 import com.labi2d.challenge.moviestwo.model.Film
 
 class FilmsAdapter(private val dataset: List<Film>) :
-    RecyclerView.Adapter<FilmsAdapter.ViewHolder>() {
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.film_title)
-    }
+    ListAdapter<Film, FilmsAdapter.ViewHolder>(basicDiffUtil { old, new -> old.name == new.name }) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val adapterLayout = LayoutInflater
-            .from(parent.context)
-            .inflate(R.layout.film_item_view, parent, false)
-
-        return ViewHolder(adapterLayout)
+        val view = parent.inflate(R.layout.film_item_view, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val film = dataset[position]
-        holder.textView.text = film.name
+        holder.bind(film)
     }
 
     override fun getItemCount(): Int = dataset.size
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = FilmItemViewBinding.bind(view)
+        fun bind(film: Film) = with(binding) {
+            filmTitle.text = film.name
+        }
+    }
 }
