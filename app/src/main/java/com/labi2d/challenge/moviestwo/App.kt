005855853481df719e20cf0aaea2bf97042eb63a
lumec <lumec@engineer.com>
2022-10-31 18:04:57 -0500

    Reverse Koin implementation

--- app/src/main/java/com/labi2d/challenge/moviestwo/App.kt
+++ app/src/main/java/com/labi2d/challenge/moviestwo/App.kt
@@ -1,11 +1,20 @@
 package com.labi2d.challenge.moviestwo
 
 import android.app.Application
+import androidx.room.Room
+import com.labi2d.challenge.moviestwo.framework.database.FilmDatabase
 
 class App : Application() {
 
+    lateinit var db: FilmDatabase
+        private set
+
     override fun onCreate() {
         super.onCreate()
-        initDi()
+
+        db = Room.databaseBuilder(
+            this,
+            FilmDatabase::class.java, "film-database"
+        ).build()
     }
 }
